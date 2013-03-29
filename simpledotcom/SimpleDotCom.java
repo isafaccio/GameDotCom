2aaaac2b01f3d56a0f1b834e823a558ebdb925ea
isafaccio <faccio.isa@gmail.com>
2013-03-28 21:45:11 -0300

    adicionando novo arquivo

---
+++ simpledotcom/SimpleDotCom.java
@@ -0,0 +1,17 @@
+public String checkYourself (String 'stringGuess) {
+	int guess = Integer.parseInt(stringGuess);
+	String result = "miss";
+	for(int cell: locationCells) {
+	if (guess == cell) {
+		result = "hit";
+		numOfHits++;
+	}
+}
+		break;
+	if(numOfHits == locationCells.lenght) {
+	result = "kill";
+}
+System.out.println(result);
+return result;
+}
+
